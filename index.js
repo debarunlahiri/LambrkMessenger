279378ccac8fbd1905bfade0be551a9d0403ac81
Debarun Lahiri <debarunlahiri2016@gmail.com>
2021-03-15 00:21:37 +0530

    Commit for bug fixes

--- index.js
+++ index.js
@@ -5,34 +5,33 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.followingNotification = functions.database.ref("/notifications/following/{searched_user_id}/{user_id}").onCreate((snap, context) => {
+exports.followingNotification = functions.database.ref("/notifications/{searched_user_id}").onCreate((snap, context) => {
   const searched_user_id = context.params.searched_user_id;
   // eslint-disable-next-line camelcase
-  const user_id = context.params.user_id;
 
-  const getFollowing = admin.database().ref(`/notifications/following/${searched_user_id}/${user_id}/user_id`).once("value");
+  const getFollowing = admin.database().ref(`/notifications/${searched_user_id}/from_user_id`).once("value");
   return getFollowing.then((result) => {
-    const user_id = result.val();
+    const from_user_id = result.val();
 
     const deviceToken = admin.database().ref(`/users/${searched_user_id}/token_id`).once("value");
 
     return deviceToken.then((result) => {
       const token_id = result.val();
 
-      const following_username = admin.database().ref(`/users/${user_id}/user_data/username`).once("value");
+      const following_username = admin.database().ref(`/users/${from_user_id}/user_data/username`).once("value");
 
       return following_username.then((result) => {
         const username = result.val();
         
-        const following_profile_image = admin.database().ref(`/users/${user_id}/user_data/thumb_profile_image`).once("value");
+        const following_profile_image = admin.database().ref(`/users/${from_user_id}/user_data/thumb_profile_image`).once("value");
 
         return following_profile_image.then((result) => {
           const image = result.val();
 
           const payload = {
             "data": {
-              "user_id": user_id,
-              "searched_user_id": searched_user_id,
+              "from_user_id": from_user_id,
+              "to_user_id": searched_user_id,
               "title": username,
               "body": "MessageChannel",
               "type": "follow_notification",
@@ -41,7 +40,7 @@ exports.followingNotification = functions.database.ref("/notifications/following
           };
 
           return admin.messaging().sendToDevice(token_id, payload).then((response) => {
-            return console.log(username + " has followed you" + "message" + image + user_id + searched_user_id);
+            return console.log(username + " has followed you " + " image: " + image + " from_user_id: " + from_user_id + " searched_user_id: " + searched_user_id + " token_id: " + token_id);
           });
         });
       });
@@ -75,8 +74,8 @@ exports.messageNotification = functions.database.ref("/notifications/chats/{rece
 
           const payload = {
             "data": {
-              "user_id": sender_user_id,
-              "receiver_user_id": receiver_user_id,
+              "from_user_id": receiver_user_id,
+              "to_user_id": sender_user_id,
               "title": username,
               "body": message,
               "type": "message_notification",
@@ -91,6 +90,92 @@ exports.messageNotification = functions.database.ref("/notifications/chats/{rece
       });
     });
   });
+});
+
+exports.storyLikeNotification = functions.database.ref("/stories/{user_id}/{story_id}/likes/{liked_user_id}").onCreate((snap, context) => {
+  const user_id = context.params.user_id;
+  const liked_user_id = context.params.liked_user_id;
+  const story_id = context.params.story_id;
+
+
+  const deviceToken = admin.database().ref(`/users/${user_id}/token_id`).once("value");
+
+  return deviceToken.then((result) => {
+    const token_id = result.val();
+
+    const following_username = admin.database().ref(`/users/${liked_user_id}/user_data/username`).once("value");
+
+    return following_username.then((result) => {
+      const username = result.val();
+
+      const following_profile_image = admin.database().ref(`/users/${liked_user_id}/user_data/thumb_profile_image`).once("value");
+
+      return following_profile_image.then((result) => {
+        const image = result.val();
+
+        const payload = {
+          "data": {
+            "from_user_id": user_id,
+            "to_user_id": liked_user_id,
+            "title": username,
+            "body": "has liked your story",
+            "type": "story_notification",
+            "icon": image,
+          },
+        };
+
+        return admin.messaging().sendToDevice(token_id, payload).then((response) => {
+          return console.log("story like " + " user_id: " + user_id + " liked_user_id: " + liked_user_id + " story_id: " + story_id + " profile_image: " + image);
+        });
+      });
+    });
+  });
+});
+
+exports.storyCommentNotification = functions.database.ref("/stories/{user_id}/{story_id}/comments/{story_comment_id}").onCreate((snap, context) => {
+  const user_id = context.params.user_id;
+  const story_id = context.params.story_id;
+  const story_comment_id = context.params.story_comment_id;
+
+  const storyComment = admin.database().ref(`/stories/${user_id}/${story_id}/comments/${story_comment_id}/comment`).once("value");
+  return storyComment.then((result) => {
+    const comment = result.val();
+
+    const storyCommentUserId = admin.database().ref(`/stories/${user_id}/${story_id}/comments/${story_comment_id}/user_id`).once("value");
+    return storyCommentUserId.then((result) => {
+      const comment_user_id = result.val();
+
+      const deviceToken = admin.database().ref(`/users/${user_id}/token_id`).once("value");
+      return deviceToken.then((result) => {
+        const token_id = result.val();
+
+        const following_username = admin.database().ref(`/users/${comment_user_id}/user_data/username`).once("value");
+        return following_username.then((result) => {
+          const username = result.val();
+
+          const following_profile_image = admin.database().ref(`/users/${comment_user_id}/user_data/thumb_profile_image`).once("value");
+          return following_profile_image.then((result) => {
+            const image = result.val();
+
+            const payload = {
+              "data": {
+                "from_user_id": user_id,
+                "to_user_id": comment_user_id,
+                "title": username,
+                "body": comment,
+                "type": "story_comment_notification",
+                "icon": image,
+              },
+            };
+
+            return admin.messaging().sendToDevice(token_id, payload).then((response) => {
+              return console.log("story comment " + " user_id: " + user_id + " story_comment_id: " + story_comment_id + " story_id: " + story_id + " profile_image: " + image);
+            });
+          });
+        });
+      });
+    });
+  });  
 });
 
 // // Create and Deploy Your First Cloud Functions
